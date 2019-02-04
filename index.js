4bd00b1e879022a627910720c84ff62d6738cb0f
Gessy90 <gessicasantoro90@hotmail.it>
2019-02-04 15:21:10 +0000

    Ready for dpeloyment

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv'). config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser= require('body-parser')
+
+
+
+const app = express()
+
+mongoose.connect('mongodb://localhost/chocolateapp')
+
+const chocolateSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  ingredients: {type: String, required: true},
+  price: {type: Number},
+  description: {type: String, required: true}
+})
+
+
+const Chocolate = mongoose.model('Chocolate', chocolateSchema)
+
+app.use(bodyParser.json())
+
+app.get('/chocolates', (req,res) => {
+  Chocolate
+    .find()
+    .then(chocolates => res.status(200).json(chocolates))
+})
+
+app.post('/chocolates',(req,res)=>{
+  Chocolate
+    .create(req.body)
+    .then(chocolates => res.status(201).json(chocolates))
+    .catch(err => res.status(422).json(err.errors))
+})
+
+app.use(express.static(`${__dirname}/dist`))
+app.get('/*', (req,res) => res.sendFile(`${__dirname}/dist/index.html`))
+app.listen(process.env.PORT, () => console.log(`Running on port ${process.env.PORT}`))
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
+      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve('src'),
+    hot: true,
+    open: true,
+    port: 8000,
+    watchContentBase: true,
+    proxy: {
+      '/api/chocolate': {
+        target: 'http://localhost:4000',
+        secure: false
+      }
+    }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+}
